d07a85c11e3c376ae5352a7832ac1d4a15abdd53
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-02-01 15:51:23 -0300

    add: error handling modified problem details factory

--- EasyLiving.Api/Program.cs
+++ EasyLiving.Api/Program.cs
@@ -1,5 +1,7 @@
+using EasyLiving.Api.Commom.Errors;
 using EasyLiving.Application;
 using EasyLiving.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,12 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
 
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, EasyLivingProblemDetailsFactory>();
 }
 
 
 var app = builder.Build();
 
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
